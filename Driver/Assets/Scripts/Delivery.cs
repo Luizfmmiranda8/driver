2161bb1b8f4954f4d24ab6c132fec34fb226f0e7
Luizfmmiranda8 <luizfmmiranda@gmail.com>
2023-03-06 15:38:04 -0300

    (feat) Change color
    Change the color of the car when pick the package and when delivery the package

--- Driver/Assets/Scripts/Delivery.cs
+++ Driver/Assets/Scripts/Delivery.cs
@@ -6,7 +6,16 @@ public class Delivery : MonoBehaviour
 {
     #region VARIABLES
     bool hasPackage;
+    SpriteRenderer spriteRenderer;
     [SerializeField] float destroyDelay = 0.5f;
+    [SerializeField] Color32 hasPackageColor = new Color32(1,1,1,1);
+    [SerializeField] Color32 noPackageColor = new Color32(1,1,1,1);
+    #endregion
+    #region EVENTS
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
     #endregion
     #region METHODS
     private void OnCollisionEnter2D(Collision2D other) 
@@ -22,11 +31,13 @@ public class Delivery : MonoBehaviour
             Debug.Log("Package picked up");
             hasPackage = true;
             Destroy(other.gameObject, destroyDelay);
+            spriteRenderer.color = hasPackageColor;
         }
         else if(other.tag == "Customer" && hasPackage)
         {
             Debug.Log("Package delivered");
             hasPackage = false;
+            spriteRenderer.color = noPackageColor;
         }   
     }
     #endregion
