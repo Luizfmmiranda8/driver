a63398c6573417e26f519864ff6a7191bbfaef95
Luizfmmiranda8 <126125049+Luizfmmiranda8@users.noreply.github.com>
2023-03-06 16:19:07 -0300

    Game logic (#5)
    * (feat) create package and customer logic
    
    Create logic to know what the player is colliding
    
    * (fix) Add regions on older code
    
    * (feat) Delivery logic
    
    Pick one package per time and delivery only if has one package
    
    * (feat) Change color
    
    Change the color of the car when pick the package and when delivery the package
    
    * (feat) Move speed
    
    Creat boost and bump system and increase the number of package in the scene

--- Driver/Assets/Scripts/FollowCamera.cs
+++ Driver/Assets/Scripts/FollowCamera.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
 {
+    #region VARIABLES
     [SerializeField] GameObject carToFollow;
+    #endregion
+
+    #region EVENTS
     //Camera position should be the same as the car's position
     void LateUpdate()
     {
        transform.position =  carToFollow.transform.position + new Vector3(0,0,-10);
     }
+    #endregion
 }
